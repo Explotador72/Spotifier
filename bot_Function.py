17437b9e5f300bd6728c46033e527fa2c9980d09
Explotador72 <83897947+Explotador72@users.noreply.github.com>
2025-07-03 16:12:22 +0200

    Add files via upload

--- bot_Function.py
+++ bot_Function.py
@@ -4,19 +4,28 @@ import asyncio
 
 from dotenv import load_dotenv
 from discord import app_commands
-from Gears.Ids import CMusic, CDiscord
+from Gears.Ids import CMusic, CDiscord, BLog
 load_dotenv()
 
 Client_secret = os.getenv('CLIENT_SECRET')
 Refresh_token = os.getenv('REFRESH_TOKEN')
 
 from Functions.Music import spotifier
+
 #Start bot
 TOKEN = os.getenv('Token')
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 Guild = discord.Object(id=CDiscord)
 tree = app_commands.CommandTree(bot)
+
+
+@bot.event
+async def on_ready():
+  await tree.sync(guild=discord.Object(id=CDiscord))
+  channel = bot.get_channel(BLog)
+  await channel.send("connected")
+  print("Ready!")
 
 
 
@@ -40,6 +49,16 @@ async def Incorrect_channel(ctx):
 
 
 #Bot commands
+@tree.command(name="great",
+              description="Say hello to him",
+              guild=Guild)
+async def DGreat(ctx):
+  if ctx.channel != bot.get_channel(CMusic):
+    await ctx.response.send_message("Hi, I'm here to assist you")
+  else:
+    await Incorrect_channel(ctx)
+
+
 @tree.command(name="get_playlist",
               description="Download a spotify playlist",
               guild=Guild)
